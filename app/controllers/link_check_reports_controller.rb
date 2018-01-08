a305701f8a3b6104d49df9aeffa6f3fb70767af9
Seb Ashton <seb@sebashton.com>
2018-01-08 14:14:45 +0000

    Add LinkCheckReport#create

--- app/controllers/link_check_reports_controller.rb
+++ app/controllers/link_check_reports_controller.rb
@@ -2,7 +2,16 @@ class LinkCheckReportsController < ApplicationController
   before_filter :find_reportable
 
   def create
+    service = LinkCheckReportCreator.new(
+      edition: @reportable
+    )
 
+    @report = service.call
+
+    respond_to do |format|
+      format.js { render 'link_check_reports/create' }
+      format.html { redirect_to edition_url(@reportable.id) }
+    end
   end
 
 private
